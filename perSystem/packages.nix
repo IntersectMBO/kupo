040b5fb5ebc078581096fd5dda7c7115d7cad980
John Lotoski <john.lotoski@iohk.io>
2026-05-28 01:50:23 -0500

    nix: add default package linked to kupo-exe

--- perSystem/packages.nix
+++ perSystem/packages.nix
@@ -4,8 +4,12 @@
       kupo = hsPkgs.kupo;
     in
     {
-      packages.kupo = kupo.components.library;
-      packages.kupo-exe = kupo.components.exes.kupo;
+      packages = {
+        default = kupo.components.exes.kupo;
+        kupo = kupo.components.library;
+        kupo-exe = kupo.components.exes.kupo;
+      };
+
       checks.kupo-unit = kupo.checks.unit;
     };
 }
