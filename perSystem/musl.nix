9e488feea43a4b584ef9e3bd2a0eabb6f78ce99a
david amick <david@davidamick.com>
2026-05-27 23:37:16 -0700

    nix: add musl static build

---
+++ perSystem/musl.nix
@@ -0,0 +1,17 @@
+{
+  perSystem = {
+    project,
+    system,
+    lib,
+    ...
+  }: let
+    muslProject = project.projectCross.${
+      if system == "x86_64-linux"
+      then "musl64"
+      else "aarch64-multiplatform-musl"
+    };
+    muslExes = muslProject.hsPkgs.kupo.components.exes;
+  in {
+    packages.kupo-exe-musl = muslExes.kupo;
+  };
+}
