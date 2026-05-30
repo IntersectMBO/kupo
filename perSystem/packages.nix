64a2e2a1e172379605234c2f1e26d1b6549f7c79
david amick <david@davidamick.com>
2026-05-29 20:26:46 -0700

    ci: setup node integration tests, release-upload workflows

--- perSystem/packages.nix
+++ perSystem/packages.nix
@@ -8,6 +8,7 @@
         default = kupo.components.exes.kupo;
         kupo = kupo.components.exes.kupo;
         kupo-lib = kupo.components.library;
+        kupo-tests = kupo.components.tests.unit;
       };
 
       checks.kupo-unit = kupo.checks.unit;
