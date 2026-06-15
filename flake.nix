28fafc0784f1401ab9bceed10bf79b8821d8c8fd
John Lotoski <john.lotoski@iohk.io>
2026-06-14 23:02:03 -0500

    flake: add arm64 native and musl builds for oci generation

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
       imports = recursiveImports [ ./perSystem ];
       systems = [
         "x86_64-linux"
-        # "aarch64-linux"
+        "aarch64-linux"
         # "aarch64-darwin"
       ];
       perSystem = { system, ... }: {
